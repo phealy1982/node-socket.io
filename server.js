40696a23cc5cefcd6dc2891d21efdba6f9bf0dc7
phealy1982 <phealy1982@gmail.com>
2017-02-24 19:17:17 +0000

    yo

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('yoClick', function () {
+  console.log('got a click');
+    socket.broadcast.emit('hideBox');
+  });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
